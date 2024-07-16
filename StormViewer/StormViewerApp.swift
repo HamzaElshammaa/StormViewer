140e48418d255c79a536cc4969fe25e8f749bb78
Hamza <mozamiza@gmail.com>
2024-07-16 22:58:27 +0300

    Initial Commit

---
+++ StormViewer/StormViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StormViewerApp.swift
+//  StormViewer
+//
+//  Created by Hamza Hatem on 16/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct StormViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
